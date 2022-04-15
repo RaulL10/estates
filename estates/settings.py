0239a197da51374cd9600b9a1c5cced43fe82a68
Hoang Nguyen <nguyenh1986@gmail.com>
2022-04-14 20:19:37 -0400

    I am doing

--- estates/settings.py
+++ estates/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+$0ekpiftj=bf!@5vyn&7_isyud!tft707x6#i0zjuz-1lgj3c'
+SECRET_KEY = ['']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
