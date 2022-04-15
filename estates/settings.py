78279db50613b5a13c34286a676dacbb757a8915
Hoang Nguyen <nguyenh1986@gmail.com>
2022-04-14 20:16:28 -0400

    we added some changes

--- estates/settings.py
+++ estates/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = 'django-insecure-+$0ekpiftj=bf!@5vyn&7_isyud!tft707x6#i0zjuz-1lgj3c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
