da3975882d3a653d50435a13c77641398048b7bd
Hoang Nguyen <nguyenh1986@gmail.com>
2022-04-16 14:29:26 -0400

    all the pages works. you can add delete, and view

--- main_app/models.py
+++ main_app/models.py
@@ -14,11 +14,13 @@ class Realtor(models.Model):
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
+
+
     def _str_(self):
-        return self.name
+      return self.name
 
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'pk': self.id})
+      return reverse('detail', kwargs={'pk': self.id})
 
 class House(models.Model):
     address = models.CharField(max_length=200)
@@ -28,8 +30,14 @@ class House(models.Model):
     price = models.IntegerField()
     realtors = models.ManyToManyField(Realtor)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    def _str_(self):
-        return self.address
+    
+    def __str__(self):
+     return self.name
+     
+    def get_absolute_url(self):
+     return reverse('detail', kwargs={'house_id': self.id})
+   
+  
 
 class Meta:
   ordering = ['-date']
