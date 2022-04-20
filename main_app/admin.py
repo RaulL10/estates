6699c2774c85b82ca8993433bd38cb3c707c1d79
eukeo <sixsblooms@gmail.com>
2022-04-19 18:36:36 -0700

    indentation on photo model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import House, Realtor, Listing
-from django.contrib import admin
+from main_app.models import images
 
+# Register your models here.
+from .models import House, Realtor, Listing, Photo
+from django.contrib import admin
+from .models import images   
+
+
+class AdminImages(admin.ModelAdmin):    
+    list_display = ['id_no','name','loc','image','profile']
+
+# Register your models here
 
 admin.site.register(House)
 admin.site.register(Listing)
 admin.site.register(Realtor)
+admin.site.register(Photo)
+admin.site.register(images, AdminImages)
